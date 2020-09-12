d02e81f8a65b53ef7a8cc4f69cf8b8c033a5ba43
Diana Mayhugh <dianamayhugh@gmail.com>
2020-09-12 13:30:58 -0400

    heroku deployment

--- server.js
+++ server.js
@@ -10,15 +10,14 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
+  app.use(express.static("client/build"));}
+
 // Add routes, both API and view
 app.use(routes);
 
 // Connect to the Mongo DB
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist", {useNewUrlParser: true}
-);
+  process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist", {useNewUrlParser: true, useUnifiedTopology: true}, );
 
 // Start the API server
 app.listen(PORT, function() {

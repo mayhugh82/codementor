b102ccfc79eadc46fc89195597b8d816fb4e2514
Diana Mayhugh <dianamayhugh@gmail.com>
2020-09-12 15:16:04 -0400

    update about in seed info

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -29,7 +29,7 @@ const bookSeed = [
       " React",
     ],
     about:
-      "I value teaching my students above all things.  When I was asked how I finds time for my wife and sweet puppy Charlie I replied “those things are unimportant in life!”  I will always make you a priority! jim@mycastle.com",
+      "I value teaching my students above all things.  When I was asked how I find time for my wife and sweet puppy Charlie I replied “those things are unimportant in life!”  Message me at: I will always make you a priority! jim@mycastle.com",
   },
   {
     name: "Curtis Lane",
@@ -53,7 +53,7 @@ const bookSeed = [
       "Spanish too!",
     ],
     about:
-      "Curtis wasn't able to fill this out because was helping all the students in our bootcamp.  Let's face it, this application would not even be up and running if it weren't for him! Thanks for always being there - anyone who wants/needs a great mentor contact Curtis!  curtis@lifecoach.com",
+      "Curtis wasn't able to fill this out because he was helping all of the students in our bootcamp.  Let's face it, this application would not even be up and running if it weren't for him! Thanks for always being there - anyone who wants/needs a great mentor contact Curtis!  Message him at: curtis@lifecoach.com",
   },
   {
     name: "Tim Fisher",
@@ -69,20 +69,19 @@ const bookSeed = [
       " React",
     ],
     about:
-      "Tim knows all the hot keys and can teach you all the short cuts!  If you’re looking for a very beardy guy – he’s your mentor! tim@bigbeards.com",
+      "I know all the hot keys and can teach you all the short cuts!  If you’re looking for a very beardy guy – I'm your mentor!   Message me at: tim@bigbeards.com",
   },
   {
     name: "Julian Villalba",
-    email: "Dr.V@swollenhand.com",
     languages: [" HTML", " ,", " Javascript", " ,", " JQuery"],
     about:
-      "Julian enjoys mentoring students about the coding world when he is not doctoring.  He is super friendly and always happy to help! Dr.V@swollenhand.com",
+      "I enjoy mentoring students about the coding world when I am not doctoring.  I am super friendly and always happy to help! Message me at: Dr.V@swollenhand.com",
   },
   {
     name: "Dana Nobile",
     languages: [" HTML", " ,", " CSS", " ,", " Javascript", " ,", " JQuery"],
     about:
-      "Dana is a front end expert!  She will help you get that fabulous looking and perfectly functioning website you have always wanted.  She might even throw in a free yoga lesson if you do a good job! Dana@dogmom.com",
+      "I am a front end expert!  I will help you get that fabulous looking and perfectly functioning website you have always wanted.  I might even throw in a free yoga lesson if you do a good job! Message me at: Dana@dogmom.com",
   },
   {
     name: "Anthony Carmack",
@@ -98,7 +97,7 @@ const bookSeed = [
       " React",
     ],
     about:
-      "Anthony is a very busy man, but if you need back end help and you can find a time to squeeze into his schedule he will get your apps running!  He is highly experienced and is worth the odd hours to work with him. anthony@twins4life.com",
+      "Anthony is a very busy man (he had his secretary write this up for him), but if you need back end help and you can find a time to squeeze into his schedule he will get your apps running!  He is highly experienced and is worth the odd hours to work with him. Message him at: anthony@twins4life.com",
   },
   {
     name: "Jackie Geiger",
@@ -114,7 +113,7 @@ const bookSeed = [
       " React",
     ],
     about:
-      "Jackie is a great mentor and can help you figure out all the bugs.  She has EXPERT google foo skills and is a wealth of knowledge. jackie@chihuahuasrock.com",
+      "I am a great mentor and can help you figure out all the bugs.  I have EXPERT google foo skills and am a wealth of knowledge. Message me at: jackie@chihuahuasrock.com",
   },
   {
     name: "Andy Edstrom",
@@ -130,13 +129,13 @@ const bookSeed = [
       " MongoDB",
     ],
     about:
-      "Andy has helped numerous newbies find their footing in the development world.  He will quit his job to make sure you understand the ins and outs of coding! andy@vollyballobsessed.com",
+      "I have helped numerous newbies find their footing in the development world.  I will quit my job to make sure you understand the ins and outs of coding! Message me at: andy@vollyballobsessed.com",
   },
   {
     name: "Diana Mayhugh",
     languages: [" Node.js", " ,", " SQL", " ,", " MongoDB", " ,", " React"],
     about:
-      "Diana is not really sure how she ended up as a mentor on this site and is great at deleting your entire project and creating bugs that have never existed before.  If you need a challenge, she is your girl! diana@estimator.com",
+      "Diana is not really sure how she ended up as a mentor on this site and is great at deleting your entire project and creating bugs that have never existed before.  If you need a challenge, she is your girl! Message her at: diana@estimator.com",
   },
 ];
 

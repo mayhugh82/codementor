9b92b6ccb98a28068580f8131aaa081b8bf413c1
Diana Mayhugh <dianamayhugh@gmail.com>
2020-09-13 16:08:46 -0400

    clean up extra code

--- client/src/components/Jumbotron/index.js
+++ client/src/components/Jumbotron/index.js
@@ -1,17 +1,8 @@
 import React from "react";
-import coder from "./coder.jpg";
 import "./style.css";
 
-
 function Jumbotron({ children }) {
-  return (
-    <div
-      
-      className="jumbotron"
-    >
-      {children}
-    </div>
-  );
+  return <div className="jumbotron">{children}</div>;
 }
 
 export default Jumbotron;
